fc43e91e381478c2127580b038b425bdfdf3d3c6
Cristiam Herrera <cristiamherrera@gmail.com>
2019-06-11 17:43:27 -0400

    Se adiciono imagenes a las cabeeras

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -9,26 +9,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link href="https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Handlee&display=swap" rel="stylesheet">
 	<style type="text/css">
-	    .titulo{
-	        font-family: 'Luckiest Guy', cursive;
-	        /*font-size: 50px;*/
-	        /*color: #ff0000;*/
-	        color: #FFF;
-	          /*font-family: "Kanit";*/
-	          font-size: 45px;
-	          line-height: 1em;
-	          margin: 0;
-	          /*position: absolute;*/
-	          text-align: center;
-	          top: 50%;
-	          /*transform: translateY(-50%);*/
-	          /*width: 100%;*/
-	          text-shadow: 0 1px 0 #e4adad, 0 2px 0 #e1a6a6, 0 3px 0 #df9e9e, 0 4px 0 #dc9696, 0 5px 0 #da8f8f, 0 6px 0 #d78787, 0 7px 0 #d58080, 0 8px 0 #d27878, 0 0 5px rgba(237, 154, 154, 0.05), 0 -1px 3px rgba(237, 154, 154, 0.2), 0 9px 9px rgba(237, 154, 154, 0.5), 0 12px 12px rgba(237, 154, 154, 0.5), 0 15px 15px rgba(237, 154, 154, 0.5);
-	    }
-	    .contenidos{
+		.titulo{
+			font-family: 'Luckiest Guy', cursive;
+			/*font-size: 50px;*/
+			/*color: #ff0000;*/
+			color: #f4bb41;
+			/*font-family: "Kanit";*/
+			font-size: 72px;
+			line-height: 1em;
+			margin: 0;
+			/*position: absolute;*/
+			text-align: center;
+			top: 50%;
+			/*transform: translateY(-50%);*/
+			/*width: 100%;*/
+			text-shadow: .1em .1em .2em rgba(0, 0, 0, 0.6);
+		}
+		.contenidos{
 	        font-family: 'Handlee', cursive;
 	        font-size: 28px;
-	        /*color: #ff0000;*/
+	        color: #ffffff;
 	        font-weight: bolder;
 	    }
 	    #txt_tutorial {
@@ -38,13 +38,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	      z-index: +1;
 	      /*background: #ff0000;*/
 	    }
+	    .cabecera {
+	      /*background:url("http://localhost/juegos/public/img/fnd.jpg") no-repeat !important;*/
+	      /*padding-top:100px;*/
+	      /*background-size:cover;*/
+	      /*height: 100%;*/
+	      /*background-position: center;*/
+	      /*background-repeat: no-repeat;*/
+	      /*background-size: cover;*/
+	      /*font-family: 'Orbitron', sans-serif;*/
+	      background: url("<?php echo base_url('public/img/fp.jpg') ?>") no-repeat center center; 
+	       -webkit-background-size: cover;
+	       -moz-background-size: cover;
+	       -o-background-size: cover;
+	       background-size: cover;
+	    } 
 	</style>
 </head>
 <body>
 
-	<div class="jumbotron text-center">
-	  <h1>JUEGOS</h1>
-	  <p>Bienvenido a nuestra consola de juegos!</p> 
+	<div class="jumbotron text-center cabecera" >
+	  <h1 class="titulo">JUEGOS</h1>
+	  <p class="contenidos">Bienvenido a nuestra consola de juegos!</p> 
 	</div>
 
 	<div class="container">
@@ -90,7 +105,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="row">
 	    <div class="col-md-6 col-sm-12">
 	    	<div class="card">
-	    	  <img src="https://www.macgamestore.com/images_screenshots/the-cooking-game-50592.jpg" class="card-img-top" alt="...">
+	    	  <img src="<?php echo base_url('/public/img/trivia.png'); ?>" class="card-img-top" alt="...">
 	    	  <div class="card-body">
 	    	    <h5 class="card-title">TRIVIA</h5>
 	    	    <p class="card-text">
