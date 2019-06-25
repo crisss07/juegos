68fc337ff3acae2733ee49f1db0371c5b2258ef6
crisss07 <rodri07crisss@gmail.com>
2019-06-25 18:38:18 -0400

    lo

---
+++ application/views/login/login.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua.compatible" content="ie-edge">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   <style type="text/css">
+
+        #uno{
+            background-color: #48A89C;
+            height: 90px;
+        }
+
+        #dos{
+            background-color: #20897B;
+            height: 50px;
+        }
+        
+        #tres{
+            background-color: white;
+            height: 520px;
+        }
+
+        #cuatro{
+            background-color: #48A89C;
+            height: 150px;
+        }
+
+        #cinco{
+            background-color: #20897B;
+            height: 70px;
+        }
+
+        #seis{
+            background-color: white;
+            height: 90px;
+        }
+
+       
+    </style>
+
+    <title>Login</title>
+  </head>
+  <body>
+    
+    <div class="container col-12">
+      <div id="row" class="row">
+        <div id="uno" class="col-12">
+            <h1 class="col-12" align="center"></h1>
+            
+         
+        </div>
+        <div id="dos" class="col-12">
+            <h1 class="col-12" align="center"></h1>
+            
+         
+        </div>
+        <div id="tres" class="col-12" >
+            <h1 class="col-12" align="center"></h1>
+                <div class="col-md-4" style="background-color: #20897B;">
+                    <form>
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Nombre de Usuario</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuario">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Contrase&ntilde;a</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contrase&ntilde;a">
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </form> 
+                </div>          
+                         
+        </div>
+        <div id="cuatro" class="col-12">
+            <h1 class="col-12" align="center"></h1>
+            
+         
+        </div>
+        <div id="cinco" class="col-12">
+            <h1 class="col-12" align="center"></h1>
+            
+         
+        </div>
+        <div id="seis" class="col-12">
+            <h1 class="col-12" align="center"></h1>
+            
+         
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url(); ?>public/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  </body>
+</html>
